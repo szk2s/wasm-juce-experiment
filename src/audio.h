d035eda0caae7d54d0554f442a7a0cae5f65c25b
szk2s <suzukisjss@gmail.com>
2019-08-31 00:37:48 +0900

    feat($root): implement audio related functions

---
+++ src/audio.h
@@ -0,0 +1,11 @@
+//
+// Created by Satoshi Suzuki on 2019-08-31.
+//
+
+#ifndef WASM_JUCE_EXAMPLE_AUDIO_H
+#define WASM_JUCE_EXAMPLE_AUDIO_H
+
+void playTestSound();
+void initializeJuce();
+
+#endif //WASM_JUCE_EXAMPLE_AUDIO_H
