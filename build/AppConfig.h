99abe3269617e9325305102a859773cdd2c6054d
szk2s <suzukisjss@gmail.com>
2019-08-31 00:06:22 +0900

    chore($build): reconfigure build process with CMake

--- build/AppConfig.h
+++
@@ -1,3 +0,0 @@
-#define APP_VERSION_MAJOR 1
-#define APP_VERSION_MINOR 0
-#define APP_NAME "WasmJuceExample"
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,6 @@
 #include "add1.h"
-#include "AppConfig.h"
 #include <iostream>
 int main(int argc, char** argv) {
-    std::cout << "Name: " << APP_NAME << " Version: " << APP_VERSION_MAJOR << "." << APP_VERSION_MINOR << std::endl;
     std::cout << "Hello World" << std::endl;
     std::cout << "1 + 1 = " << add1(1) << std::endl;
     return 0;
